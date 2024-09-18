e08d735a421370b2c963c8ff01e4952557bc0169
0xSWE <0xSWE@tuta.io>
2024-09-18 05:40:44 +0300

    Suckless added.

---
+++ suckless/dmenu/colorschemes/catppuccin.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*               fg         bg       */
+	[SchemeNorm] = { "#7c6f64", "#32302f" },
+	[SchemeSel] = { "#32302f", "#d65d0e" },
+	[SchemeOut] = { "#000000", "#d65d0e" },
+};
